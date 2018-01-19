e47edd05e1c66777144464e5a54608b1475aa21c
Karl-Cool <karl@diskoteket.eu>
2018-01-19 13:59:27 +0100

    Fixed login with google and email, trying to get auth on every page

--- public/karlscripts.js
+++ public/karlscripts.js
@@ -15,10 +15,27 @@ var storageRef = storage.ref();
 
 mainapp.controller('mainCtrl', function ($scope) {
     $scope.postsList = [];
+    var currentUid = null;
+    firebase.auth().onAuthStateChanged(function (user) {
+        // onAuthStateChanged listener triggers every time the user ID token changes.  
+        // This could happen when a new user signs in or signs out.  
+        // It could also happen when the current user ID token expires and is refreshed.  
+        if (user && user.uid != currentUid) {
+            // Update the UI when a new user signs in.  
+            // Otherwise ignore if this is a token refresh.  
+            // Update the current user UID.  
+            currentUid = user.uid;
+            $scope.logintext = '<h1> Congrats ' + user.displayName + ', you are done! </h1>' + user.email + ' <h2> Now get back to what you love building. </h2> <h2> Need to verify your email address or reset your password? Firebase can handle all of that for you using the email you provided: ' + user.email + '. <h/2>';
+        } else {
+            // Sign out operation. Reset the current user UID.  
+            currentUid = null;
+            console.log("no user signed in");
+        }
+    });
     var imagesRef = storageRef.child('Blogphotos');
     var fileName = 'firstday.jpg';
     var spaceRef = imagesRef.child(fileName);
-    var imgurl = spaceRef.getDownloadURL().then(function(url){
+    var imgurl = spaceRef.getDownloadURL().then(function (url) {
         console.log(url);
         document.querySelector('img').src = url;
     })
@@ -92,5 +109,4 @@ mainapp.factory('resultsFactory', function ($http, $timeout, $q) {
     results.all = _all;
     return results;
 });
-
 
