cb35275eadd4c1dbc7fe121b5bb535c1897ebc29
Karl-Cool <karl@diskoteket.eu>
2018-01-19 16:16:13 +0100

    www

--- public/script.js
+++ public/script.js
@@ -19,7 +19,7 @@ angular.module('PopupDemo').controller('PopupDemoCont', ['$scope','$modal',funct
     $scope.open = function () {
     console.log('opening pop up');
     var modalInstance = $modal.open({
-    templateUrl: 'popup.html',
+    templateUrl: 'writePost.html',
     });
     }
     }]);
